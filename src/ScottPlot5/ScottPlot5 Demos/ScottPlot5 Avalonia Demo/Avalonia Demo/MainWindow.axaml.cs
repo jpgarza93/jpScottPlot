39dc9e6ad3495c4a1616bf86b1d3cecab69418c2
JP\User <jp@roboticsys.com>
2024-09-10 17:15:17 -0500

    added avalonia ui demo with linux consolas font issue

---
+++ src/ScottPlot5/ScottPlot5 Demos/ScottPlot5 Avalonia Demo/Avalonia Demo/MainWindow.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia.Controls;
+
+namespace Avalonia_Demo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Subtitle.Content = $"ScottPlot.Avalonia Version {ScottPlot.Version.VersionString}";
+
+            // testing settings
+            var plot = AvaPlot.Plot;
+            plot.Axes.Bottom.Label.FontName = "Consolas";
+            plot.Axes.Left.Label.FontName = "Consolas";
+        }
+    }
+}
